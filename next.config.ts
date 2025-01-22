06f845de0a68ef1793eb5130c90ccfdc49b6454e
70_10 <7010.dev@gmail.com>
2025-01-22 18:02:22 +0900

    Update Next.js 15 and change Page Router -> App Router

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
@@ -11,4 +11,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
--- src/pages/index.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main className="m-10">
-      <p>Top</p>
-    </main>
-  );
-}
